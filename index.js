ed9c1a3e133743e2a79f0177941e273a34b63e4d
Shaun Alexander <shaunaa126@gmail.com>
2021-03-24 19:52:47 +0000

    loop through 1k set of users

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const { WebClient } = require('@slack/web-api');
+const _ = require('lodash');
 
 const web = new WebClient(process.env.SLACK_TOKEN);
 
 async function syncUsers(options) {
-  let users = [];
+  let members = [];
 
   for await (const page of web.paginate('conversations.members', { channel: options.sourceChannel })) {
     console.log('Results of conversation.members', page)
-    users = page.members;
+    members = page.members;
   }
   
-  const response = await web.conversations.invite({
-    channel: options.targetChannel,
-    users: users.join(","),
-  })
-  console.log('Results of conversation.invite', response);
+  addUsers(options.targetChannel, members);
+}
+
+async function addUsers(channel, members) {
+  let users = _.chunk(members, 1000);
+
+  for (var i = 0; i < users.length; i++) {
+    const response = await web.conversations.invite({
+      channel: channel,
+      users: users.join(","),
+    })
+    console.log('Results of conversation.invite', response);
+  }
 }
 
 async function main(options) {
